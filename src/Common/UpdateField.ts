24a8f76df54d11e1eb7f5902645e97a6f3ac737f
Matthew Davis <matthew@matthewdavis.io>
2021-06-27 21:21:29 -0400

    bump

--- src/Common/UpdateField.ts
+++ src/Common/UpdateField.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Length } from 'class-validator';
 
 export class UpdateField {
 
     @ApiProperty()
+    @Length(1, 255)
     public name: string;
 
     @ApiProperty()
