431b573cea582ef14184765a8d85edf8733163e9
Matthew Davis <matthew@matthewdavis.io>
2021-07-25 18:43:55 -0400

    bump :pray:

--- src/Servers/ServerCreate.ts
+++ src/Servers/ServerCreate.ts
@@ -13,6 +13,10 @@ export class ServerCreate {
 
     @ApiProperty()
     @Matches(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    public plan: string;
+
+    @ApiProperty()
+    @Matches(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
     public type: string;
 
     @ApiProperty()
